436e39ebfbb4726ed3b0cf4d9bc78ace6b896951
Sabin <35765016+xplameboy@users.noreply.github.com>
2019-05-23 15:57:47 +0200

    Create Account View updated

--- src/view/CreateAccountAndLogIn/CreateAccountView.java
+++ src/view/CreateAccountAndLogIn/CreateAccountView.java
@@ -3,12 +3,17 @@ package view.CreateAccountAndLogIn;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import view.ViewHandler;
 import viewmodel.AccountAndLogIn.CreateAccountViewModel;
 import viewmodel.ViewModelProvider;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class CreateAccountView {
 
@@ -26,12 +31,20 @@ public class CreateAccountView {
     private TextField loginField;
 
     @FXML
-    private TextField passwordField;
+    private PasswordField passwordField;
 
     @FXML
     void createAccount(ActionEvent event) throws IOException {
+        LocalDate localDate = dateOfBirthField.getValue();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthDate = new Date();
+        try {
+            birthDate = format.parse(localDate.toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        viewModel.createAccount(nameField.getText(), surnameField.getText(), birthDate, loginField.getText(), passwordField.getText());
         viewModel.openCreateAccountAndLogIn();
-
     }
 
     public void init(CreateAccountViewModel createAccountViewModel) {
