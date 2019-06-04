38a2c25898e79a5cedd80b33fc2ad7bc6c1f89c7
Sabin <35765016+xplameboy@users.noreply.github.com>
2019-06-04 20:40:47 +0200

    ServerModelTest

--- src/Test/ServerModelTest.java
+++ src/Test/ServerModelTest.java
@@ -1,23 +1,76 @@
 package Test;
 
+import Client.Client;
+import Client.IRMIClient;
+import Client.RMIClient;
+import DataModel.Car;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import server.ServerModel;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import static org.junit.Assert.*;
 
 public class ServerModelTest {
 
+    ServerModel serverModel;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void start(){
+        try {
+            serverModel = new ServerModel();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
     @org.junit.Test
     public void logIn() {
+        exception.expect(RemoteException.class);
+        exception.expect(NotBoundException.class);
+        String userName = "polish";
+        String password = "pass";
+        String firstName = "Carl";
+        String lastName = "Car";
+        Date birthDate = new GregorianCalendar(1990, Calendar.FEBRUARY, 07).getTime();
+
+
+
+        try {
+            serverModel.addCustomer(userName, password, firstName, lastName, birthDate);
+            IRMIClient client = new RMIClient();
+
+            assertEquals(true, serverModel.logIn(userName, password, client));
+        } catch (RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @org.junit.Test
-    public void addCar() {
+    public void addCar() throws RemoteException {
+        Car car = new Car("A12345", "Afdff", 10, "red", 2000, "Small car", 100, 0);
+        serverModel.addCar(car);
     }
 
     @org.junit.Test
-    public void deleteCar() {
+    public void deleteCar() throws RemoteException {
+        Car car = new Car("A12345", "Afdff", 10, "red", 2000, "Small car", 100, 0);
+        serverModel.addCar(car);
+        serverModel.deleteCar(car.getRegistrationNumber());
     }
 
     @org.junit.Test
     public void editCar() {
+        serverModel.editCar();
     }
 
     @org.junit.Test
