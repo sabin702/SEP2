10b0cf950bfb7d764cebb70a73272b7f9d6ecaa0
Sabin <35765016+xplameboy@users.noreply.github.com>
2019-06-06 11:59:03 +0200

    JUnit Created

---
+++ src/Test/CustomerModelImplTest.java
@@ -0,0 +1,18 @@
+package Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerModelImplTest {
+
+    @Test
+    public void getNumberOfDays() {
+
+    }
+
+    @Test
+    public void calculateTotalPrice() {
+        
+    }
+}
